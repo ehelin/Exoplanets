39884c56798c69d2b12242bb43e47f03b48a4248
Eric Helin <ehelin@ejward.com>
2026-03-11 21:17:38 -0500

    query

--- Queries.sql
+++ Queries.sql
@@ -1,8 +1,26 @@
-﻿select * from exoplanet.change_log;
-select * from exoplanet.change_report;
+﻿
 select * from exoplanet.pipeline_log;
 select * from exoplanet.ingest_run;
+select * from exoplanet.change_log;
 select * from exoplanet.exoplanets;
+select * from exoplanet.change_report;
 
 TRUNCATE exoplanet.ingest_run CASCADE;
 TRUNCATE exoplanet.exoplanets;
+
+SELECT 
+    e.planet_name,
+    e.host_star,
+    e.discovery_year,
+    e.classification,
+    cl.change_type,
+    cl.field_name,
+    cl.old_value,
+    cl.new_value,
+    cl.ai_classification,
+    cl.ai_reasoning
+FROM exoplanet.exoplanets e
+JOIN exoplanet.change_log cl ON cl.planet_name = e.planet_name
+WHERE e.classification IS NOT NULL
+ORDER BY e.planet_name, cl.detected_at
+LIMIT 30;
